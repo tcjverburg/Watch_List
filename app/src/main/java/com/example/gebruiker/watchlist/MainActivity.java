8a00aecab80a05fce2dca29c0bcdaccbe085d5c8
tcjverburg <tom_verburg@hotmail.nl>
2016-09-21 21:05:53 +0200

    Made tutorial work JSON parser.

--- app/src/main/java/com/example/gebruiker/watchlist/MainActivity.java
+++ app/src/main/java/com/example/gebruiker/watchlist/MainActivity.java
@@ -1,13 +1,95 @@
 package com.example.gebruiker.watchlist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+//Users should be able to search for movies, read descriptions and view poster art,
+// as well as add such titles to a list of movies to watch.
+// And of course, they should be able to remove titles from the list as well!
+public class MainActivity extends Activity {
+
+    private TextView tvData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //source: https://www.youtube.com/watch?v=Gyaay7OTy-w
+        Button btnHit = (Button) findViewById(R.id.btnHit);
+        tvData = (TextView) findViewById(R.id.tvJsonItem);
+
+        btnHit.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View v) {
+                                          new JSONTask().execute("http://www.omdbapi.com/?t=gladiator");
+                                      }}
+        );
     }
+
+    public class JSONTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+                return buffer.toString();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            tvData.setText(result);
+
+        }
+    }
+
 }
