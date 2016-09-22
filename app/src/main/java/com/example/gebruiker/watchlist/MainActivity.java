c225d441054292dcebd12a69209081ccc47a0e1d
tcjverburg <tom_verburg@hotmail.nl>
2016-09-22 14:47:43 +0200

    Can search for movies and gives you Title, plot and poster.

--- app/src/main/java/com/example/gebruiker/watchlist/MainActivity.java
+++ app/src/main/java/com/example/gebruiker/watchlist/MainActivity.java
@@ -5,7 +5,11 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -19,7 +23,11 @@ import java.net.URL;
 // And of course, they should be able to remove titles from the list as well!
 public class MainActivity extends Activity {
 
-    private TextView tvData;
+    private String request;
+    private String title;
+    private String plot;
+    private String poster;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +35,27 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         //source: https://www.youtube.com/watch?v=Gyaay7OTy-w
-        Button btnHit = (Button) findViewById(R.id.btnHit);
-        tvData = (TextView) findViewById(R.id.tvJsonItem);
+        Button btnSearch = (Button) findViewById(R.id.btnHit);
 
-        btnHit.setOnClickListener(new View.OnClickListener() {
-                                      @Override
-                                      public void onClick(View v) {
-                                          new JSONTask().execute("http://www.omdbapi.com/?t=gladiator");
-                                      }}
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUserInput();
+                new JSONTask().execute("http://www.omdbapi.com/?t="+request);
+
+            }}
         );
-    }
 
+    }
+    public String getUserInput()
+    {
+        EditText inputText = (EditText) findViewById((R.id.userInput));
+        return request = String.valueOf(inputText.getText()).replaceAll(" ", "+");
+    }
+    public void test(){
+        TextView textView = (TextView)findViewById(R.id.tvJsonItem1);
+        textView.setText(plot+"gelukt");
+    }
     public class JSONTask extends AsyncTask<String, String, String> {
 
         @Override
@@ -87,7 +105,28 @@ public class MainActivity extends Activity {
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-            tvData.setText(result);
+
+            TextView textview = (TextView) findViewById(R.id.tvJsonItem1);
+            TextView textview2 = (TextView) findViewById(R.id.tvJsonItem2);
+            ImageView imageView = (ImageView) findViewById(R.id.poster);
+
+            String movie = result.substring(1,result.length()-1);
+            if (movie.startsWith("\"Response\":\"False\",\"Error\"")){
+                textview.setText(movie);
+            }
+            else {
+                String[] parts = movie.split("\",\"");
+                String title = parts[0].replaceAll("\"", "");
+                String plot = parts[9].replaceAll("\"", "");
+                String poster = parts[13].replaceAll("\"", "").substring(7);
+
+                textview.setText(title);
+                textview2.setText(plot);
+
+                Picasso.with(getApplicationContext())
+                        .load(poster)
+                        .into(imageView);
+            }
 
         }
     }
